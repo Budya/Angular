3299b4d63b2d049c09a77a659c2ff1b46ad8e4d3
Budya <budya2012@gmail.com>
2019-07-26 09:00:19 +0300

    templateVariables done

---
+++ 09_templateVariables/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+       
+// @Component({
+//     selector: 'my-app',
+//     template: `
+//                 <p #userName>{{name}}</p>
+//                 <p>{{userName.textContent}}</p>
+//                 <input type="text" [(ngModel)]="name" />`
+// })
+// export class AppComponent { 
+//     name:string="Tom";
+// }
+
+      
+@Component({
+    selector: 'my-app',
+    template: `<child-comp #counter></child-comp>
+                <button (click)="counter.increment()">+</button>
+                <button (click)="counter.decrement()">-</button>`
+})
+export class AppComponent { }
