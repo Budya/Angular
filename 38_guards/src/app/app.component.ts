264d34c10854a5098419a9de615eafd57f612c40
Budya <budya2012@gmail.com>
2019-08-07 16:58:22 +0300

    guards done

---
+++ 38_guards/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component} from '@angular/core';
+   
+@Component({
+    selector: 'my-app',
+    template: `<div>
+                    <h1>Маршрутизация в Angular 2</h1>
+                    <nav>
+                        <a routerLink="">Главная</a>
+                        <a routerLink="/about">О сайте</a>
+                    </nav>
+                    <router-outlet></router-outlet>
+               </div>`
+})
+export class AppComponent {}
